5806b85c1c6a36b1eb1ffa6149db75bbdcdedff8
Jonathas <contato@jonathas.com>
2016-02-10 16:00:14 +0100

    Implemented colors for the grid

--- js/grid.js
+++ js/grid.js
@@ -1,5 +1,14 @@
-$(document).ready(function() {
-    for(i=0; i < 100; i++) {
+function generateGrid() {
+	var numSquares = 2013;
+
+	for(i=0; i < numSquares; i++) {
   		$('#grid').append('<div class="griditem"' + 'id="griditem_' + i +'">' + '</div>')
+  		
+  		
+  		$('#griditem_' + i).css('background-color', randomColor());
 	}
+}
+
+$(document).ready(function() {
+	generateGrid();
 });
