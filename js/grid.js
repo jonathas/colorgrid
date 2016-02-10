02dbecd87bb09665462ae7a719ac1c118c50ffb0
Jonathas <contato@jonathas.com>
2016-02-10 16:25:52 +0100

    Implemented random color generation for the grid items

--- js/grid.js
+++ js/grid.js
@@ -1,12 +1,15 @@
 function generateGrid() {
-	var numSquares = 2013;
+	var numSquares = 2030;
+	var randomSquare = 0;
 
 	for(i=0; i < numSquares; i++) {
-  		$('#grid').append('<div class="griditem"' + 'id="griditem_' + i +'">' + '</div>')
-  		
-  		
-  		$('#griditem_' + i).css('background-color', randomColor());
+  		$('#grid').append('<div class="griditem"' + 'id="griditem_' + i +'">' + '</div>');
+
+  		randomSquare = Math.floor(Math.random() * numSquares);
+
+  		$('#griditem_' + randomSquare + 20).css('background-color', randomColor());
 	}
+	
 }
 
 $(document).ready(function() {
