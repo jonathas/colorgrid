e859d2dbc6f25b701ef6c3c8fade6d41a82b7517
Jonathas <contato@jonathas.com>
2016-02-10 17:33:08 +0100

    Implemented the undo button

--- js/grid.js
+++ js/grid.js
@@ -1,10 +1,13 @@
 var selectedColor = "";
+var actionHistory = [];
 
 function generateGrid() {
 	var numSquares = 2030;
 	var randomSquare = 0;
 
-	for(i=0; i < numSquares; i++) {
+	$('#grid').html('');
+
+	for(var i = 0; i < numSquares; i++) {
   		$('#grid').append('<div class="griditem"' + 'id="griditem_' + i +'">' + '</div>');
 
   		randomSquare = Math.floor(Math.random() * numSquares);
@@ -28,6 +31,19 @@ function pickColor(element) {
 function colorGridItem(element) {
 	var id = $(element.target).attr("id");
 	$('#' + id).css('background-color', selectedColor);
+
+	actionHistory.push({id:id,color:selectedColor});
+}
+
+function undoColorGridItem() {
+	if(actionHistory.length === 0) {
+		return;
+	}
+
+	var last = actionHistory[actionHistory.length-1];
+
+	$('#' + last.id).css('background-color', '');
+	actionHistory.pop();
 }
 
 $(document).ready(function() {
@@ -35,5 +51,13 @@ $(document).ready(function() {
 
 	$('.griditem').click(function(e) {
 		pickColor(e);
+	});
+
+	$('#undo').click(function(e) {
+		undoColorGridItem();
+	});
+
+	$('#reset').click(function(e) {
+		location.reload();
 	});
 });
